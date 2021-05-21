1c39cdcac851a933b6947250a4946ecc8fcb3819
Craig Tran <craigtran@craigs-imac.lan>
2021-05-21 15:21:18 -0700

    Initializing start of project.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Git Flow</h1>
+      </header>
+      <p>
+        Welcome to the Git Flow project playground.
+      </p>
+    </div>
+  );
+}
+
+export default App;
